27f10072938ce742109d70465f85ed063a7ea9ee
MalulekiNyuswa <malunyz@yahoo.com>
2016-07-13 13:08:53 +0200

    Added the serverjs file for the backend server

---
+++ server.js
@@ -0,0 +1,14 @@
+
+var express = require("express");
+//var jwt = require("express-jwt");
+//var cors = require("cors");
+
+var app = express();
+var port = 3200;
+
+app.get("/", function(req, res){
+    res.send("BACK-END SERVER RUNNING ON PORT 3200");
+});
+
+app.listen(port);
+console.log("Listening on port " + port);
