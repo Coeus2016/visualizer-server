cb80303e9f1f0d52b00eb8b0b846911bbf511944
MalulekiNyuswa <malunyz@yahoo.com>
2016-07-20 09:55:07 +0200

    Added the earthquakes model

---
+++ models/earthquakes_model.js
@@ -0,0 +1,62 @@
+module.exports = {
+    type: "FeatureCollection",
+        metadata:
+    {
+        generated: Long_Integer,
+        url: String,
+        title: String,
+        api: String,
+        count: Integer,
+        status: Integer
+    },
+    bbox: [
+        'minimum longitude',
+        'minimum latitude',
+        'minimum depth',
+        'maximum longitude',
+        'maximum latitude',
+        'maximum depth'
+    ],
+    features:
+    [
+        {
+            type: "Feature",
+            properties: {
+                mag: Decimal,
+                place: String,
+                time: Long_Integer,
+                updated: Long_Integer,
+                tz: Integer,
+                url: String,
+                detail: String,
+                felt: Integer,
+                cdi: Decimal,
+                mmi: Decimal,
+                alert: String,
+                status: String,
+                tsunami: Integer,
+                sig: Integer,
+                net: String,
+                code: String,
+                ids: String,
+                sources: String,
+                types: String,
+                nst: Integer,
+                dmin: Decimal,
+                rms: Decimal,
+                gap: Decimal,
+                magType: String,
+                type: String
+            },
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    longitude,
+                    latitude,
+                    depth
+                ]
+            },
+            id: String
+        },
+]
+};
