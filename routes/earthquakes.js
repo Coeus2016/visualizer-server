731e807d60baff0f14814c5b6d453c7504f6291d
MalulekiNyuswa <malunyz@yahoo.com>
2016-07-22 16:05:08 +0200

    Modified the earthquake route - removed server scrapping code

--- routes/earthquakes.js
+++ routes/earthquakes.js
@@ -7,61 +7,7 @@ module.exports = function(app) {
     var bluebird = require("bluebird");
 
     var database = "earthquakes";
-
-    // USGS earthquake data feed url
-    var feedUrl = "earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
-
-    // Fetch data from USGS, transform locations into point objects.
-    // Insert the data into the 'earthquakes' table.
-    var refresh = r.table("quakes_table").insert(r.http(feedUrl)("features").merge(function(item) {
-        return {
-            geometry: r.point(item("geometry")("coordinates")(0),
-                              item("geometry")("coordinates")(1))
-        }
-    }), {conflict: "replace"});
-
-
-    // Initial setup, creating the database and table and geospatial
-    // index on the 'geometry' property, access the above querry and
-    // populate the table with data
-    var conn;
-    r.connect(config.database).then(function(c) {
-        conn = c;
-        return r.dbCreate(config.database.db).run(conn);
-    }).then(function() {
-        return r.tableCreate("quakes").run(conn);
-    }).then(function() {
-        return r.table("quakes").indexCreate(
-            "geometry", {geo: true}).run(conn);
-    }).then(function() {
-        return refresh.run(conn);
-    }).error(function(err) {
-        if(err.msg.indexOf("already exists") == -1)
-            console.log(err);
-    }).finally(function () {
-        if(conn)
-            conn.close();
-    });
-
-
-    // Use the refresh query above to automatically update the 'earthquake' database
-    // with new data at 30 minutes intervals and delete the records that are older than 30 days
-    setInterval(function() {
-        var connect;
-        r.connect(config.database).then(function (c) {
-            connect = c;
-
-            return bluebird.join(refresh.run(connect), r.table("quakes")
-                .filter(r.epochTime(r.row("properties")("time").div(1000)).lt(
-                r.now().sub(60 * 60 * 24 * 30))).delete().run(connect));
-        }).error(function(err) {
-            console.log("Failed to refresh:", err);
-        }).finally(function() {
-            if (connect)
-                connect.close();
-        });
-    }, 30 * 1000 * 60);
-
+    
     // Define the `/earthquakes` endpoint for the backend API. It querries the "earthquake" database
     // and retrieves the earthquakes ordered by magnitude and then returns the output as a JSON array
     app.get("/quakes", function (req, res) {
@@ -104,8 +50,8 @@ module.exports = function(app) {
                 console.log("Error handling /nearest request:", err);
                 res.status(500).json({err: err});
             }).finally(function () {
-                if(conn)
-                    conn.close();
+            if(conn)
+                conn.close();
         });
     });
 
