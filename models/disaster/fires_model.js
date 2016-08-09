bcf2b7d9d829bbf83814b6bd588ca6617a030723
u12163555 <kamo.tswene@gmail.com>
2016-08-09 14:09:18 +0200

    Updated fires model

--- models/disaster/fires_model.js
+++ models/disaster/fires_model.js
@@ -1,5 +1,5 @@
 ///@file fires_model.js
-/// model for storing fire data found at source: https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_Global_24h.csv
+/// Fires model
 module.exports = {
         [
             {
@@ -19,35 +19,3 @@ module.exports = {
             }
         ]
 };
-
-
-/*module.exports = {
-    type: "FeatureCollection",
-    features:
-        [
-            {
-                type: "Feature",
-                properties: {
-                    brightness: Decimal,
-                    scan: Decimal,
-                    track: Decimal,
-                    acq_date: Date,
-                    acq_time: Long_Integer,
-                    satellite: String,
-                    confidence: Integer,
-                    version: String,
-                    bright_t31: Decimal,
-                    frp: Decimal,
-                    daynight: String
-                },
-                geometry: {
-                    type: "Point",
-                    coordinates: [
-                        longitude,
-                        latitude
-                    ]
-                },
-                id: String
-            },
-        ]
-};*/
