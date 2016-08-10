6e8ca029df06a90323650c018eceb8bf2d04ea08
MalulekiNyuswa <malunyz@yahoo.com>
2016-08-10 10:38:09 +0200

    Added the earthquake controller

--- controller/disaster/earthquakes.js
+++ controller/disaster/earthquakes.js
@@ -1,65 +1,32 @@
-///@file earthquake.js
-/// Controller containing functionality for earthquakes 
+'use strict';
 
-var config = require('../../Javascript/rethinkdb_config.js');
+module.exports = function(app) {
 
-var r = require("rethinkdb");
-var bluebird = require("bluebird");
+    var droughts = require('../../controller/disaster/drought');
+    var dust_storms = require('../../controller/disaster/dust_storms');
+    var earthquakes = require('../../controller/disaster/earthquakes');
+    var fires = require('../../controller/disaster/fires');
+    var floods = require('../../controller/disaster/floods');
+    var severe_storms = require('../../controller/disaster/severe_storms');
 
-var database = "earthquakes";
+    // Return droughts json object
+    app.route('/droughts').get();
 
-/**
-* @function exports.findEarthquakes
-*Defines the `/earthquakes` endpoint for the backend API. It queries the "earthquake" database
-* and retrieves the earthquakes ordered by magnitude and then returns the output as a JSON array
-* @param {object} req object containing  information about HTTP request 
-* @param  {object} res the desired HTTP response
-*/
- exports.findEarthquakes = function (req, res) {
-    var conn;
-    r.connect(config.database).then(function(c) {
-        conn = c;
+    // Rreturn dust_storms json object
+    app.route('/dust_storms').get();
 
-        return r.table("quakes").orderBy(
-            r.desc(r.row("properties")("mag"))).run(conn);
-    }).then(function(cursor){ return cursor.toArray();}).then(
-        function (result) { res.json(result);}).error(function (err) {
-        console.log("Error handling /quakes request:", err);
-        res.status(500).json({success: false, err: err});
-    }).finally(function() {
-        if(conn)
-            conn.close();
-    });
+    // Rreturn earthquakes json object
+    app.route('/earthquakes').get(earthquakes.findEarthquakes);
+    //app.route('/earthquakes:start').get(earthquakes.findEarthquakes);
+
+    // Rreturn fires json object
+    app.route('/fires').get();
+
+    // Rreturn floods json object
+    app.route('/floods').get();
+
+    // Rreturn severe_storms json object
+    app.route('/severe_storms').get();
+
 };
 
-/**
- * @function exports.findNearestEarthquakes
- * Defines the '/nearest' endpoint for the backend API.  It queries the 'earthquakes' table to find the closest
- * earthquake to the longitude and latitude specified, which then returns the result as a JSON object
- * @param {object} req Takes two URL query parameters, representing the lattitude and longitude of a point.
- * @param {object} res the desired HTTP response
- */
- exports.findNearestEarthquakes = function (req, res) {
-    var latitude = req.param("latitude");
-    var longitude = req.param("longitude");
-
-    if(!latitude || !longitude)
-        return res.status(500).json({ err: "Invalid Point"});
-
-    var conn;
-
-    r.connect(config.database).then(function(c) {
-        conn = c;
-
-        return r.table("quakes").getNearest(
-            r.point(parseFloat(longitude), parseFloat(latitude)),
-            { index: "geometry", maxDist: 1000, unit: "mi"}).run(conn);
-    }).then(function (result) { res.json(result);}).error(
-        function (err) {
-            console.log("Error handling /nearest request:", err);
-            res.status(500).json({err: err});
-        }).finally(function () {
-        if(conn)
-            conn.close();
-    });
-};
