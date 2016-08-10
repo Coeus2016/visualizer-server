d0eea232fe50ced506673d59bf780f63c978667d
MalulekiNyuswa <malunyz@yahoo.com>
2016-08-10 10:43:43 +0200

    Modified the disaster route

--- routes/disaster/disaster.js
+++ routes/disaster/disaster.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-/// @file disaster.js
-/// Stores all the routes that will be required  
->>>>>>> 57439c74e7be4e620ca705ae024f53d9fcaf94cc
 'use strict';
 
 module.exports = function(app) {
@@ -14,7 +9,6 @@ module.exports = function(app) {
     var floods = require('../../controller/disaster/floods');
     var severe_storms = require('../../controller/disaster/severe_storms');
 
-<<<<<<< HEAD
     // Return droughts json object
     app.route('/droughts').get();
 
@@ -35,27 +29,4 @@ module.exports = function(app) {
     app.route('/severe_storms').get();
 
 };
-
-=======
-    /// Return droughts json object
-    app.route('/droughts').get();
-
-    /// Return dust_storms json object
-    app.route('/dust_storms').get();
-
-    /// Return earthquakes json object
-    app.route('/earthquakes').get(earthquakes.findEarthquakes);
-    //app.route('/earthquakes:start').get(earthquakes.findEarthquakes);
-
-    /// Return fires json object
-    app.route('/fires').get(fires.findFires);
-
-    /// Return floods json object
-    app.route('/floods').get();
-
-    /// Return severe_storms json object
-    app.route('/severe_storms').get();
-
-};
->>>>>>> 57439c74e7be4e620ca705ae024f53d9fcaf94cc
 
