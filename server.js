24c7cc102d4a7d3402bb3dbb50dd629f008c8561
keletso <patrickkeletso@gmail.com>
2016-08-29 10:27:06 +0200

    fix cross side scripting

--- server.js
+++ server.js
@@ -2,24 +2,26 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
+var cors = require('cors');
 
 //Parse application/json and look for raw text
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));
 
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
 
 var port = 3300;
 
 //routes
-//require('./routes/disaster/disaster')(app);//pass our application into our disaster route
-//require('./routes/weather/weather')(app);//weather route
+require('./routes/disaster/disaster')(app);//pass our application into our disaster route
+require('./routes/weather/weather')(app);//weather route
 
 //require('./routes/users')(app);
 
-//require('./scrapper-server');
+require('./scrapper-server');
 //require('../visualizer-scrapper/fireScrapData');
 
 //start app
