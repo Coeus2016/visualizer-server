f06c1a273995383459817e9c16ce76155ccc5323
MalulekiNyuswa <malunyz@yahoo.com>
2016-10-13 11:16:47 +0200

    Added the floods controller

--- controller/disaster/floods.js
+++ controller/disaster/floods.js
@@ -1,1 +1,34 @@
+var config = require('../../Javascript/rethinkdb_config.js');
 
+var r = require("rethinkdb");
+var bluebird = require("bluebird");
+
+var database = "earthquakes";
+var table = "floodAreas";
+
+/** Define the `/floods` endpoint for the backend API. It querries the "earthquake" database
+ * and retrieves the earthquakes ordered by magnitude and then returns the output as a JSON array
+ *
+ * @param req
+ * @param res
+ */
+exports.findFloodAreas = function (req, res) {
+    var conn;
+    r.connect(config.database).then(function(c) {
+        conn = c;
+
+        return r.table(table).orderBy(
+            r.desc(r.row('items')('fwdCode'))).run(conn);
+    }).then(function(cursor){ return cursor.toArray();}).then(
+        function (result) { res.json(result);}).error(function (err) {
+        console.log("Error handling /quakes request:", err);
+        res.status(500).json({success: false, err: err});
+    }).finally(function() {
+        if(conn)
+            conn.close();
+    });
+};
+
+
+
+ 
