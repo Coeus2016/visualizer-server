42ac4820371051374fd281c6c0717cbf2c95b758
MalulekiNyuswa <malunyz@yahoo.com>
2016-10-24 00:02:51 +0200

    Modified the earthquakes unit tests

--- test/controllers.disasters.earthquakes.test.js
+++ test/controllers.disasters.earthquakes.test.js
@@ -19,6 +19,8 @@ var should = chai.should();
 var sinonChai = require('sinon-chai');
 //chai.use(sinonChai);
 
+var token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hbHVueXpAeWFob28uY29tIiwiZmlyc3RfbmFtZSI6Ik1hbHVsZWtpIiwibGFzdF9uYW1lIjoiTnl1c3dhIn0.ek9iV_iUiPIPNmFsjlHAEqA2UQ-8XjK-8BZo3DNCjx0';
+
 var server = require('../server');
 var earthquake = require('../controller/disaster/earthquakes');
 //var quakesTable = require('../models/disaster/earthquakes_model');
@@ -30,14 +32,14 @@ describe('EarthQuakeTest', function() {
     var conn;
     beforeEach(function (done) { // Before each test we empty the database
         console.log('before test!');
-        
+
         db.connect(config.database).then(function(c) {
              conn = c;
-            db.table("quakes").delete();
+           // db.table("quakes").delete();
             done();
         })
     })
-    
+
     after(function (done) {
         db.dbDrop(config.database);
             conn.close();
@@ -48,10 +50,11 @@ describe('EarthQuakeTest', function() {
      * Test the GET /earthquakes route
      */
     describe('/GET All Earthquakes', function () {
-        it('it should GET all earthquakes as a json object', function (done) {
+        it('it should POST all earthquakes', function (done) {
             try {
                 chai.request(server)
-                    .get('/earthquakes')
+                    .post('/filteredquakes')
+		                .set('Authorization', token )
                     .end(function (err, res) {
                         res.should.have.status(200);
                         res.body.should.be.a('array');
@@ -63,67 +66,153 @@ describe('EarthQuakeTest', function() {
             }
         });
 
-        /**
-         * Test the GET /inbetweenEarthquakes route
-         */
-        it('should GET all earthquakes that are between specified first and second epochTimes as a json object', function (done) {
-            try {
-                chai.request(server)
-                    .get('/inbetween/1471602678470/1471605529000')
-                    .end(function (err, res) {
-                        res.should.have.status(200);
-                        res.body.should.be.a('array');
-                        //res.body.length.should.be.eql(1595);
-                        res.body.should.include({"geometry":{"$reql_type$":"GEOMETRY","coordinates":[-67.3945,18.7155],"type":"Point"},"id":"pr16232001","properties":{"alert":null,"cdi":null,"code":"16232001","detail":"http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/pr16232001.geojson","dmin":0.33866486,"felt":null,"gap":205.2,"ids":",pr16232001,","mag":2.6,"magType":"Md","mmi":null,"net":"pr","nst":13,"place":"39km NW of San Antonio, Puerto Rico","rms":0.46,"sig":104,"sources":",pr,","status":"REVIEWED","time":1471604037700,"title":"M 2.6 - 39km NW of San Antonio, Puerto Rico","tsunami":0,"type":"earthquake","types":",cap,geoserve,origin,","tz":-240,"updated":1471605177708,"url":"http://earthquake.usgs.gov/earthquakes/eventpage/pr16232001"},"type":"Feature"});
-                        done();
-                    });
-            }catch(e) {
-                done(e);
-            }
-        });
+      /**
+       * Setting the filter
+       */
+      it('it should POST a response after inserting a filter into the database', function (done) {
+        try {
+          chai.request(server)
+            .post('/earthquakefilter')
+            .set('Authorization', token )
+            .send({
+              "filter":"{\"location\": -1,\"date\" : 4,\"magnitude\" : 1}"
+            })
+            .end(function (err, res) {
+              //console.log(res.body);
+              done();
+            });
+        }catch(e) {
+          done(e);
+        }
+      });
 
 
-        /**
-         * Test the GET /lessthanEarthquakes route
-         */
+      /**
+       * Test the POST filter by location
+       */
+      it('it should POST all earthquakes filtered by location as a json object', function (done) {
+        try {
+          chai.request(server)
+            .post('/filteredquakes')
+            .set('Authorization', token )
+            .send({
+              longitude: -149.953,
+              latitude: 61.4714
+            })
+            .end(function (err, res) {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              done();
+            });
+        }catch(e) {
+          done(e);
+        }
+      });
 
-        it('should GET all earthquakes before the specified epochTime as a json object', function (done) {
-            try {
+      /**
+       * Test the POST filter by date; all earthquakes that occured in the last hour
+       */
+      it('it should POST all earthquakes filtered by time; that occurred in the last hour as a json object', function (done) {
+        try {
+          chai.request(server)
+            .post('/filteredquakes')
+            .set('Authorization', token )
+            .send({
+              date: 0
+            })
+            .end(function (err, res) {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              done();
+            });
+        }catch(e) {
+          done(e);
+        }
+      });
 
-                chai.request(server)
-                    .get('/lessthan/1469623035490')  //27/07/2016 17:37:15
-                    .end(function (err, res) {
-                        res.should.have.status(200);
-                        res.body.should.be.a('array');
-                        //res.body.length.should.be.eql(0);
-                        done();
-                    });
-            }catch(e) {
-                done(e);
-            }
-        });
+      /**
+       * Test the POST filter by date; all earthquakes occuring today
+       */
+      it('it should POST all earthquakes filtered by date; occuring today as a json object', function (done) {
+        try {
+          chai.request(server)
+            .post('/filteredquakes')
+            .set('Authorization', token )
+            .send({
+              date: 1
+            })
+            .end(function (err, res) {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              done();
+            });
+        }catch(e) {
+          done(e);
+        }
+      });
 
+      /**
+       * Test the POST filter by date; all earthquakes that occured in the last 24 hours
+       */
+      it('it should POST all earthquakes filtered by date; occurred in the last 24 hours as a json object', function (done) {
+        try {
+          chai.request(server)
+            .post('/filteredquakes')
+            .set('Authorization', token )
+            .send({
+              date: 2
+            })
+            .end(function (err, res) {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              done();
+            });
+        }catch(e) {
+          done(e);
+        }
+      });
 
-        /**
-         * Test the GET /greatorthanEarthquakes route
-         */
-        it('should GET all earthquakes after the specified epochTime as a json object', function (done) {
-            try {
-                chai.request(server)
-                    .get('/greatorthan/1471601958000')   //19/08/2016 12:19:18
-                    .end(function (err, res) {
-                        res.should.have.status(200);
-                        res.body.should.be.a('array');
-                        //res.body.length.should.be.eql(25752);
-                        done();
-                    });
-            }catch(e) {
-                done(e);
-            }
-        });
-////////?""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+      /**
+       * Test the POST filter by date; all earthquakes that occured in the last 48 hours
+       */
+      it('it should POST all earthquakes filtered by date; occured in the last 48 hours as a json object', function (done) {
+        try {
+          chai.request(server)
+            .post('/filteredquakes')
+            .set('Authorization', token )
+            .send({
+              date: 3
+            })
+            .end(function (err, res) {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              done();
+            });
+        }catch(e) {
+          done(e);
+        }
+      });
 
-
+      /**
+       * Test the POST filter by date; all earthquakes that occured in the last 7 days
+       */
+      it('it should POST all earthquakes filtered by date; occured in the last 7 days as a json object', function (done) {
+        try {
+          chai.request(server)
+            .post('/filteredquakes')
+            .set('Authorization', token )
+            .send({
+              date: 4
+            })
+            .end(function (err, res) {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              done();
+            });
+        }catch(e) {
+          done(e);
+        }
+      });
 
 
     });
